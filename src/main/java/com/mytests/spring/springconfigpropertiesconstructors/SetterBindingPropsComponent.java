105269b8ba89e9133cde7ce05d87dba35772a9b6
Irina.Petrovskaya <irina.petrovskaya@jetbrains.com>
2026-03-30 13:36:34 +0200

    comments

--- src/main/java/com/mytests/spring/springconfigpropertiesconstructors/SetterBindingPropsComponent.java
+++ src/main/java/com/mytests/spring/springconfigpropertiesconstructors/SetterBindingPropsComponent.java
@@ -6,27 +6,27 @@ import org.springframework.stereotype.Component;
 
 // this class is defined as configuration properties using @ConfigurationProperties @Componenyt class;
 // despite the constructor presence, the setters should be used to bind properties
-// no error should be shown
+
 @ConfigurationProperties(prefix = "setter.component.props")
 @Component
 public class SetterBindingPropsComponent {
 
     String field1; // this field is binded (n/r by IDEA)
     String field2; // this field is binded (n/r by IDEA)
-    String field3; // this field is NOT binded via ctor, but IDEA thinks it is
-    String field4; // this field is NOT binded via ctor, but IDEA thinks it is
+    SomeBean3 field3; // this field is NOT binded via ctor, but IDEA thinks it is
+    SomeBean4 field4; // this field is NOT binded via ctor, but IDEA thinks it is
 
 
-    public SetterBindingPropsComponent(String field3, String field4) {
+    public SetterBindingPropsComponent(SomeBean3 field3, SomeBean4 field4) {
         this.field3 = field3;
         this.field4 = field4;
     }
 
-    public String getField3() {
+    public SomeBean3 getField3() {
         return field3;
     }
 
-    public String getField4() {
+    public SomeBean4 getField4() {
         return field4;
     }
 
@@ -45,4 +45,13 @@ public class SetterBindingPropsComponent {
     public void setField1(String field1) {
         this.field1 = field1;
     }
+}
+
+@Component
+class SomeBean3 {
+
+}
+@Component
+class SomeBean4 {
+
 }
